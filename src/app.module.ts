811e6612c1648b0b860f286238391e66df17ab80
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-02-25 20:29:02 +0600

    added modules to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
 
-@Module({})
+@Module({
+  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+})
 export class AppModule {}
