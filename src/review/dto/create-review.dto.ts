04326669e66163140f6c56960fbd3726719dbce0
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-01 00:19:10 +0600

    added validation

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly title: string;
+
+  @IsString()
   readonly description: string;
+
+  @Max(5, { message: 'Рейтинг не может быть больше 5' })
+  @Min(1, { message: 'Рейтинг не может быть менее 1' })
+  @IsNumber()
   readonly rating: number;
+
+  @IsString()
   readonly productId: string;
 }
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -6,7 +6,7 @@ import {
   HttpException,
   HttpStatus,
   Param,
-  Post,
+  Post, UsePipes, ValidationPipe,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
@@ -16,6 +16,7 @@ import { REVIEW_NOT_FOUND } from './review.constants';
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('create')
   public async create(@Body() dto: CreateReviewDto) {
     return await this.reviewService.createProduct(dto);
