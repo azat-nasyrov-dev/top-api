78f746fb0355c038bcb47cb81430ddea4ea2b7eb
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-02 01:59:04 +0600

    added a custom decorator

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -15,6 +15,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { UserEmail } from '../decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -36,8 +37,10 @@ export class ReviewController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
-  public async getByProduct(@Param('productId') productId: string) {
+  public async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
+    console.log(email);
     return await this.reviewService.findByProductId(productId);
   }
 }
