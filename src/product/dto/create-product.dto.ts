f8abc6aec3097a7e72e92f4f38ae7f1d87ec57e2
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-04 01:01:50 +0600

    added dto for product

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,50 @@
+import { IsArray, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+class ProductCharacteristicDto {
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  readonly value: string;
+}
+
+export class CreateProductDto {
+  @IsString()
+  readonly image: string;
+
+  @IsString()
+  readonly title: string;
+
+  @IsNumber()
+  readonly price: number;
+
+  @IsOptional()
+  @IsNumber()
+  readonly oldPrice?: number;
+
+  @IsNumber()
+  readonly credit: number;
+
+  @IsString()
+  readonly description: string;
+
+  @IsString()
+  readonly advantages: string;
+
+  @IsString()
+  readonly disAdvantages: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  readonly categories: string[];
+
+  @IsArray()
+  @IsString({ each: true })
+  readonly tags: string[];
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => ProductCharacteristicDto)
+  readonly characteristics: ProductCharacteristicDto[];
+}
