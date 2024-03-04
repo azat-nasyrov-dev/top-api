fc5a51b3dcb14f4e9374c03a4ce817d9c37f4939
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-05 04:10:49 +0600

    added dto for top-page

---
+++ src/top-page/dto/update-top-page.dto.ts
@@ -0,0 +1,62 @@
+import { TopLevelCategory } from '../models/top-page.model';
+import { IsArray, IsEnum, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class HhDataDto {
+  @IsNumber()
+  readonly count: number;
+
+  @IsNumber()
+  readonly juniorSalary: number;
+
+  @IsNumber()
+  readonly middleSalary: number;
+
+  @IsNumber()
+  readonly seniorSalary: number;
+}
+
+export class TopPageAdvantageDto {
+  @IsString()
+  readonly title: string;
+
+  @IsString()
+  readonly description: string;
+}
+
+export class UpdateTopPageDto {
+  @IsEnum(TopLevelCategory)
+  readonly firstCategory: TopLevelCategory;
+
+  @IsString()
+  readonly secondCategory: string;
+
+  @IsString()
+  readonly alias: string;
+
+  @IsString()
+  readonly title: string;
+
+  @IsString()
+  readonly category: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => HhDataDto)
+  readonly hh?: HhDataDto;
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => TopPageAdvantageDto)
+  readonly advantages: TopPageAdvantageDto[];
+
+  @IsString()
+  readonly seoText: string;
+
+  @IsString()
+  readonly tagsTitle: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  readonly tags: string[];
+}
