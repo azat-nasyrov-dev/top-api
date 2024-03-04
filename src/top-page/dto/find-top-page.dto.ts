e63a909fa65c9f6a55d54293b5fe66a1dfda1eff
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-05 04:11:10 +0600

    added validation to dto

--- src/top-page/dto/find-top-page.dto.ts
+++ src/top-page/dto/find-top-page.dto.ts
@@ -1,5 +1,7 @@
 import { TopLevelCategory } from '../models/top-page.model';
+import { IsEnum } from 'class-validator';
 
 export class FindTopPageDto {
+  @IsEnum(TopLevelCategory)
   readonly firstCategory: TopLevelCategory;
 }
