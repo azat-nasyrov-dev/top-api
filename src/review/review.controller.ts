48507994181a71235f34acbf7ca5e88afb331dae
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-07 16:59:37 +0600

    integrated telegraph into the app

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,15 +16,32 @@ import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { IdValidationPipe } from '../pipes/id-validation.pipe';
+import { TelegramService } from '../telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly telegramService: TelegramService,
+  ) {}
 
   @UsePipes(new ValidationPipe())
   @Post('create')
   public async create(@Body() dto: CreateReviewDto) {
     return await this.reviewService.createProduct(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('notify')
+  public async notify(@Body() dto: CreateReviewDto) {
+    const message =
+      `Имя: ${dto.name}\n` +
+      `Заголовок: ${dto.title}\n` +
+      `Описание: ${dto.description}\n` +
+      `Рейтинг: ${dto.rating}\n` +
+      `ID Продукта: ${dto.productId}`;
+
+    return await this.telegramService.sendMessage(message);
   }
 
   @UseGuards(JwtAuthGuard)
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,9 +3,10 @@ import { ReviewController } from './review.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ReviewModel, ReviewSchema } from './models/review.model';
 import { ReviewService } from './review.service';
+import { TelegramModule } from '../telegram/telegram.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: ReviewModel.name, schema: ReviewSchema }])],
+  imports: [MongooseModule.forFeature([{ name: ReviewModel.name, schema: ReviewSchema }]), TelegramModule],
   controllers: [ReviewController],
   providers: [ReviewService],
 })
